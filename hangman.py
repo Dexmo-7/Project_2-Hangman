ea42de6a0890f95f8631724082c0742c949eb26f
Mateusz Owsianka <mowsianka13@gmail.com>
2020-09-13 01:56:56 +0200

    New loop provided.

--- hangman.py
+++ hangman.py
@@ -10,6 +10,7 @@
 
 #Starting conditions
 game_set = True
+game_con = True
 counter = 0
 
 #Stars appearing
@@ -18,31 +19,33 @@ listPasswd = list("Zbigniew Wodecki".upper())
 strUnpasswd = ''.join(["*" if item.isalpha() else ' ' for item in listPasswd])
 print(strUnpasswd)
 
+while game_con:
+    print(""
+    while game_set:
 
-while game_set:
-    #Checking loop for main condition if n==letter_from_passwd
-    listUnpasswd = list(strUnpasswd)
-    n = input("Enter a letter: ").upper()
-    innerCounter = 0
-    for num, item in enumerate(listPasswd):
-        if n==item:
-            listUnpasswd[num] = listPasswd[num]
-            innerCounter += 1
+        #Checking loop for main condition if n==letter_from_passwd
+        listUnpasswd = list(strUnpasswd)
+        n = input("Enter a letter: ").upper()
+        innerCounter = 0
+        for num, item in enumerate(listPasswd):
+            if n==item:
+                listUnpasswd[num] = listPasswd[num]
+                innerCounter += 1
 
-    strUnpasswd = ''.join(listUnpasswd)
-    print(strUnpasswd)
+        strUnpasswd = ''.join(listUnpasswd)
+        print(strUnpasswd)
     
-    #Confirmation that gamer choose good letter, if not then increase the counter
-    if innerCounter == 0:
-        counter += 1
-        print(f"Damn, you made {counter}/7 bad decision...")
+        #Confirmation that gamer choose good letter, if not then increase the counter
+        if innerCounter == 0:
+            counter += 1
+            print(f"Damn, you made {counter}/7 bad decision...")
     
-    #Checking if counter is lower than 7, if not then game is over
-    if counter == 7:
-        game_set = False
-        print("Oh no! You lost!")
+        #Checking if counter is lower than 7, if not then game is over
+        if counter == 7:
+            game_set = False
+            print("Oh no! You lost!")
     
-    if "*" not in listUnpasswd:
-        game_set = False
-        print("Congratulation! You won!")
+        if "*" not in listUnpasswd:
+            game_set = False
+            print("Congratulation! You won!")
     
