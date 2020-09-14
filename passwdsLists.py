e658b1e7c081da2076fe5f43d23143c496717d38
Mateusz Owsianka <mowsianka13@gmail.com>
2020-09-14 15:00:27 +0200

    Module with passwords for guessing.

---
+++ passwdsLists.py
@@ -0,0 +1,9 @@
+#lists with passwds
+listWithSingers = [
+    "zbigniew wodecki",
+    "elton john",
+    "scott mckenzie",
+    "johnny cash",
+    "richard chamberlain",
+    "michael jackson"
+    ]
